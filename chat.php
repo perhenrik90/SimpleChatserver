d3588d0958ec17a86112d328892aff3b24379c05
Per-Henrik2 <perhenrik.kvalnes@gmail.com>
2014-07-23 22:26:52 +0200

    chat.php now loads messages form the database

--- chat.php
+++ chat.php
@@ -2,15 +2,36 @@
 <html>
 <meta charset='utf8'></meta>
 <link rel="stylesheet" type="text/css" href="css/main.css"></link>
+<link rel="stylesheet" type="text/css" href="css/chat.css"></link>
 <body>
-<h1>Welcome to Simple Chat</h1>
+<h1 class="mainH1">Simple Chat</h1>
 
 <div class="blanket">
-   
+   <div id="chatArea">
+   <?php
+   require('database.php');
+   $c = dbGetChatCollection();
+
+$result = $c->find();
+
+foreach($result as $post)
+  {
+    $message = $post["message"];
+    $nn = $post["nickname"];
+
+    $p = "<div class='chatRow'>"
+      ."<p class='nicknameString'>$nn</p>"
+      ."<p class='messageString'>$message</p>" 
+      ."</div></br>";
+
+    echo $p;
+  }
+   ?>
+   </div>
 </div>
 <?php
 
-require('database.php');
+
 
 $nickname = $_COOKIE["nickname"];
 echo $nickname;
