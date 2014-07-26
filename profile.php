ecd1f0c11fd8e32a4afe33a2dada34616b8e06d3
Per-Henrik2 <perhenrik.kvalnes@gmail.com>
2014-07-26 21:46:37 +0200

    profile shows age and name if the data is in database

--- profile.php
+++ profile.php
@@ -11,13 +11,35 @@
 <div class="blanket">
    <div class="mainInformation">
    <?php
-   $userid = $_GET["userid"];
+   $userid = $_GET["nickname"];
 if(! $userid)
   {
     echo "<h1>No user seleced </h1>";
     echo "<p><a href='chat.php'>Go back to chat</a></p>";
+    die();
   }
+require('database.php');
+
+$collection = dbGetUserCollection();
+$cursor = $collection->find(array('nickname'=>$userid));
+$usr = $cursor->getNext();
+
+if(! $usr)
+  {
+    echo "<h1>User not found!</h1>";
+    echo "<p>No user found with nickname ";
+    echo "<p class='redText'> $userid</p></p>";
+    die();
+  }
+
+$name = $usr["name"];
+$age  = $usr["age"];
+
 echo "<p>$userid</p>";
+
+
+if($name){ echo "<p>Name: $name</p>";}
+if($age){ echo "<p>Age: $age</p>";}
 
 ?>
   </div>
